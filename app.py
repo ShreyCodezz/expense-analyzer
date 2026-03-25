e0d2f2916a5912307fba177248026483d5892d2b
Shreyaan Bhat <149713753+ShreyCodezz@users.noreply.github.com>
2026-03-26 00:53:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="Expense Analyzer", page_title="💰")
+st.set_page_config(page_title="Expense Analyzer", icon="💰")
 
 st.title("💰 AI Expense Analyzer")
 
